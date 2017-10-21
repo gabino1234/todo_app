d0f549e8d4dc5f0b6e637beb0e22ad51fe3388ca
gabino rodriguez <gabino.rodriguez@epicacademy.org>
2017-10-21 19:42:52 +0000

    add links to index page

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,8 +3,17 @@ class TodoController < ApplicationController
     def index
     end
     def show 
+        todo_id= params[:id]
+        if todo_id=='1' # Remenber,the'==''
         @todo_description = "make the curriculum"
-        @todo_pomodoro_estimate = 4 
-end
-
+        @todo_pomodoro_estimate = 40 
+        
+    
+        elsif todo_id=='2'
+            @todo_description = "Buy workshop supplies"
+            @todo_pomodoro_estimate = 50
+        
+        
+        end
+ end
 end
